3f2278027e2411c3b904ae3eb3200f39c96b776f
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-03-26 10:23:24 +0530

    Added expressions execution and input from user in python

--- python/main.py
+++ python/main.py
@@ -2,3 +2,25 @@ a=10
 b=20
 sum = a+b
 print("Sum of a and b is: ",sum)
+
+
+# *Expression Execution
+A,B="2",3
+TEXT = "&"
+print(2*TEXT*3)
+
+A,B = "2",3
+TEXT = "@"
+print((A+TEXT)*B)
+
+
+# Input from user in python
+
+name = input("Enter your name: ")
+print("Hello",name)
+
+age = int(input("Enter your age: "))
+print("Your age is: ",age)
+
+price = float(input("Enter the price: "))
+print("Price is: ",price)
