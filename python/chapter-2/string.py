928fa802ee181272af1d1e41ab6cc0fd634d4b9b
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-03-27 10:36:22 +0530

    Refactor code and remove commented out lines

---
+++ python/chapter-2/string.py
@@ -0,0 +1,27 @@
+str1 = "this is a string \n this is a new line"
+str2='ApnaCollege'
+str3='''This is a multiline string'''
+
+# basic operations on strings
+
+# concatenation
+print(str1 + str2)
+
+# length of string
+print(len(str1))
+
+
+
+
+# indexing
+str = "SIGMA_DEVELOPER"  # 0(S) 1(I) 2(G) 3(M) 4(A) 5(_) 6(D) 7(E) 8(V) 9(E) 10(L) 11(O) 12(P) 13(E) 14(R)
+
+
+# slicing
+print(str[0:5])  # SIGMA
+print(str[6:])   # DEVELOPER
+print(str[:5])   # SIGMA
+print(str[:])    # SIGMA_DEVELOPER
+print(str[-1])   # R
+print(str[-3:])  # PER
+print(str[:-3])  # SIGMA_DEVELOP
--- python/main.py
+++ python/main.py
@@ -1,26 +1,86 @@
-a=10
-b=20
-sum = a+b
-print("Sum of a and b is: ",sum)
+# #  a = 10
+# # b = 20
+# # sum = a + b
+# # print("Sum of a and b is: ", sum)
 
 
-# *Expression Execution
-A,B="2",3
-TEXT = "&"
-print(2*TEXT*3)
+# # # *Expression Execution
+# # A, B = "2", 3
+# # TEXT = "&"
+# # print(2 * TEXT * 3)
 
-A,B = "2",3
-TEXT = "@"
-print((A+TEXT)*B)
+# # A, B = "2", 3
+# # TEXT = "@"
+# # print((A + TEXT) * B)
 
 
-# Input from user in python
+# # # Input from user in python
 
-name = input("Enter your name: ")
-print("Hello",name)
+# # #* name = input("Enter your name: ")
+# # # 
+# print("Hello", name)
 
-age = int(input("Enter your age: "))
-print("Your age is: ",age)
+# age = int(input("Enter your age: "))
 
-price = float(input("Enter the price: "))
-print("Price is: ",price)
+# # # print("Your age is: ", age)
+
+# # # price = float(input("Enter the price: "))
+# # # print("Price is: ", price)
+
+# # # Conditional Statements
+
+# # # if 10 > 5:
+# # #     print("10 is greater than 5")
+# # # elif 5 > 2:
+# # #     print("5 is greater than 2")
+
+# # # else:
+# # #     print("None of the above")
+
+
+# # light = input("Enter the color of the light: ")
+# # if light == "red":
+# #     print("Stop")
+# # elif light == "yellow":
+# #     print("Wait")
+# # elif light == "green":
+# #     print("Go")
+# # else:
+# #     print("Invalid color")
+
+
+
+# # # single line condiitonal statements
+# # # <expression> if <condition> else <expression>
+# # food = input("food : ")
+# # eat = "Yes" if food == "Pizza" else "No"
+
+# # # clever way to write the above code
+# # age = int(input("Enter your age: "))
+# # vote = ("yes","no")[age < 18]
+
+# # # relational operators
+# # a=50
+# # b=20
+
+
+# # print(a == b)
+# # print(a != b)
+
+# # # LOGICAL OPERATORS
+# # A=50
+# # B=30
+# # print(not False)
+
+
+# # type conversion
+# a=2
+# b=4.25
+
+# sum = a + b
+
+
+num1 = int(input("Enter number_1 "));
+num2 = int(input("Enter number_2 "));
+
+print(num1+num2)
