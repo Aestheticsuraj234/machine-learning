d80785c3b4aeb5b41e02dc71edebd8881dc02932
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-03-31 20:34:07 +0530

    Refactor functions.py and add new functions

--- python/chapter-6/functions.py
+++ python/chapter-6/functions.py
@@ -1,5 +1,52 @@
-# make functions in pyton
-def Calc_Sum(a,b):
-    sum = a+b
-    return sum
-print(Calc_Sum(2,3))
+# # make functions in pyton
+# def Calc_Sum(a,b):
+#     sum = a+b
+#     return sum
+# print(Calc_Sum(2,3))
+
+# cities = ['delhi' , 'bengaluru' , 'gurgaon' , 'hyedrabad']
+
+# def print_len(city):
+#      print(len(city))
+
+
+# def print_city(city):
+#     for item in list:
+#         print(item)
+
+
+n = 5
+
+# for i in range(1 , n+1):
+#     fact *= i
+# print(fact)
+
+
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1 , n+1):
+#         fact *= i
+#     return fact
+
+
+# print(cal_fact(5))
+
+
+def isOdd():
+    n = int(input("Enter a number: "))
+    if n % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+isOdd()
+
+
+def show(n):
+    if n == 0:
+        return
+    print(n)
+    show(n - 1)
+
+show(5)
