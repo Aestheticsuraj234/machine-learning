dacdd09240714f5be1d1ee3518fea8339a8e0a25
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-03-31 20:09:34 +0530

    Fix bug in login functionality

---
+++ python/chapter-5/loops.py
@@ -0,0 +1,62 @@
+# # # # while loops
+# # # count = 1;
+# # # while count <= 5:
+# # #     print("I'm stuck in an infinite loop!")
+# # #     count += 1
+    
+    
+    
+# # num =1
+# # while num <=100:
+# #     print(num)
+# #     num +=1
+    
+    
+        
+# # num =100
+# # while num >=1:
+# #     print(num)
+# #     num -=1
+
+
+# n=2
+# count = 1
+
+# while count<=10:
+#     print( str(n) + " x " + str(count) + " = " + str(n*count))
+#     count +=1
+
+
+# nums = [1,4,9,16,25,36,49,64,81,100]
+
+
+# INDEX = 0
+# while INDEX < len(nums):
+#     print(nums[INDEX])
+#     INDEX +=1
+    
+
+# TUP = (1,2,3,4,5,6,7,8,9,10)
+
+# i = 0;
+# while i < len(TUP):
+#     if(TUP[i] ==8):
+#       print("Number 8 found" , TUP[i]) 
+#     i += 1    
+    
+    
+# list = [1,2,3]
+# veggies = ["potato","brijal","tomato"]
+# for val in veggies:
+#     print(val)    
+# else:
+#     print("No items left.")    
+    
+
+
+for i in range(1,11):
+    print(i)
+    
+
+for i in range(10,0,-1):
+    print(i)    
