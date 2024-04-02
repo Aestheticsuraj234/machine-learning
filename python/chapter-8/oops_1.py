4b9c4c148462713e26cf946fa0672dcf532de66d
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-04-02 10:33:26 +0530

    Add Student class constructor and create an instance with a given name

--- python/chapter-8/oops_1.py
+++ python/chapter-8/oops_1.py
@@ -1,8 +1,23 @@
 class Student:
-    name="Rahul Kumar"
+    def __init__(self , fullname):
+        self.name=fullname
+        print(self.name,"Created")
+   
 
-
-s1=Student()
-s2=Student()
+s1=Student("Suraj")
+# s2=Student()
 print(s1.name)
-print(s2.name)    
+# print(s2.name)    
+
+
+# class Car:
+#     color="Red"
+#     brand="BMW"
+#     engine="V8"
+    
+    
+    
+# c1 = Car()
+# c2 = Car()
+# print(c1.color)
+# print(c2.color)    
