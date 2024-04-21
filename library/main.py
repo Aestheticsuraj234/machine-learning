caeceb33a774fcecbb2c31aa450bd8b47eec785c
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-04-22 02:02:59 +0530

    Refactor main.py to use numpy random function

--- library/main.py
+++ library/main.py
@@ -1,41 +1,5 @@
 import numpy as np
 
-# # myArr = np.array([[1, 2, 3, 4, 5]] , np.int32)
- 
-# # print(myArr.shape)
+data = [np.random.standard_normal() for i in range(7)]
 
-
-# # # Array creation: Conversion from other Python structures (e.g., lists, tuples)
-
-# # listArray = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-
-# # # print(listArray.shape)
-# # # print(listArray.size)
-# # zeros = np.zeros((2,5))
-
-# # print(zeros)
-
-# # rng = np.arange(15)
-
-# # print(rng)
-
-
-# # lspace = np.linspace(1,50,10)
-
-# # print(lspace)
-
-
-# # emp = np.empty((4,6))
-
-# # print(emp)
-# # ide = np.identity(45)
-# # print(ide)
-
-# arr = np.arange(99)
-
-# print(arr)
-
-# arr = arr.reshape(3,33)
-
-# print(arr)
-
+print(data)
